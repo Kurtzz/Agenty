3efad773590b6ea4fc583e9ef5133386a7806eec
Kurtzz <przemyslaw.kurc@gmail.com>
2017-06-23 01:31:41 +0200

    Move ClientService to separate module, add functionality

--- client/src/main/scala/pl/edu/agh/agenty/learning/client/ClientService.scala
+++ client/src/main/scala/pl/edu/agh/agenty/learning/client/ClientService.scala
@@ -4,21 +4,24 @@ package pl.edu.agh.agenty.learning.client
   * Created by Przemyslaw Kurc on 2017-06-22.
   */
 
-import io.grpc._
-import pl.edu.agh.agenty.learning.grpc._
 import java.util.concurrent.TimeUnit
 
 import com.google.protobuf.ByteString
+import io.grpc._
 import org.slf4j.LoggerFactory
-import pl.edu.agh.agenty.learning.utils.Consts
+import pl.edu.agh.agenty.learning.grpc._
 
 object ClientService {
   private val logger = LoggerFactory.getLogger(classOf[ClientService].getName)
+  private val multiplier = 100
 
   @throws[Exception]
   def main(args: Array[String]): Unit = {
     val client = new ClientService("localhost", 6565)
     client.train()
+    client.recognize()
+    client.reset()
+    client.shutdown()
   }
 }
 
@@ -38,10 +41,23 @@ class ClientService {
   }
 
   def train(): Unit = {
-    val value = new Array[Byte](Consts.NUM_PIXELS)
-    val labels = new Array[Byte](1)
-    var request: TrainBatchRequest = TrainBatchRequest.newBuilder().setCount(1).setLabels(ByteString.copyFrom(labels)).setPixels(ByteString.copyFrom(value)).build()
+    val value = new Array[Byte](Consts.NUM_PIXELS*multiplier)
+    val labels = new Array[Byte](1*multiplier)
+    var request: TrainBatchRequest = TrainBatchRequest.newBuilder().setCount(1*multiplier).setLabels(ByteString.copyFrom(labels)).setPixels(ByteString.copyFrom(value)).build()
     var response: TrainResponse = blockingStub.trainBatch(request)
     logger.info("response" + response)
+  }
+
+  def recognize(): Unit = {
+    val value = new Array[Byte](Consts.NUM_PIXELS)
+    var request = ClassifyRequest.newBuilder().setPixels(ByteString.copyFrom(value)).build()
+    var response = blockingStub.classifyImageSoftmax(request)
+    logger.info("response " + response.getResult)
+  }
+
+  def reset(): Unit = {
+    var request = ResetRequest.getDefaultInstance
+    var response = blockingStub.reset(request)
+    logger.info("response " + response)
   }
 }
